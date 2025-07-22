2ce8824487e415cf2be0cb99204110bb1f4ead8f
Shinsuke Tano <shinsuke@sMacmini.local>
2025-07-22 22:13:36 +0900

    Update deploy-app.yml variable.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,13 +8,13 @@ variable "project_id" {
 variable "region" {
   description = "The GCP region to deploy resources into."
   type        = string
-  default     = "asia-northeast1" // 東京リージョン
+  default     = "asia-northeast1" // ★★★実習では、australia-southeast1★★★
 }
 
 variable "zone" {
   description = "The GCP zone to deploy resources into."
   type        = string
-  default     = "asia-northeast1-a"
+  default     = "asia-northeast1-a" // ★★★実習では、australia-southeast1-a★★★
 }
 
 variable "gke_num_nodes" {
@@ -24,7 +24,7 @@ variable "gke_num_nodes" {
 }
 
 variable "outdated_linux_image" {
-  description = "An outdated Linux image for the MongoDB VM (Debian 10)."
+  description = "An outdated Linux image for the MongoDB VM (Debian 11)."
   type        = string
   default     = "debian-cloud/debian-11"
 }
