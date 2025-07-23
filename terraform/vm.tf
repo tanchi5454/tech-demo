2341f320b5baa3476b59dfd4ca3442bd6e904a1b
tanchi5454 <28645989+tanchi5454@users.noreply.github.com>
2025-07-23 18:52:24 +0900

    Update vm.tf mongodb password

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -25,7 +25,7 @@ resource "google_secret_manager_secret" "mongodb_password" {
 }
 resource "google_secret_manager_secret_version" "mongodb_password_version" {
   secret      = google_secret_manager_secret.mongodb_password.id
-  secret_data = "Shtano+07012025" # ★★★ 必ず強力なパスワードに変更 ★★★
+  secret_data = "wizpassword" # ★★★ 必ず強力なパスワードに変更 ★★★
 }
 
 #  JWTトークン用のSecret Key
