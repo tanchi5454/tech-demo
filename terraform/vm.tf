b42c59cf732e3c339d7c2a5e651b2dde3aa5c7c5
tanchi5454 <28645989+tanchi5454@users.noreply.github.com>
2025-07-24 12:15:34 +0900

    add startup-script.sh for mongodb-vm

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -96,7 +96,7 @@ resource "google_compute_instance" "mongodb_vm" {
   # 起動スクリプトでSecret Managerから認証情報を取得
   metadata = {
     # file() 関数で外部スクリプトファイルを指定
-    startup-script = file("startup_script.sh")
+    startup-script = file("startup-script.sh")
   }
 
   tags = ["mongodb-vm"]
