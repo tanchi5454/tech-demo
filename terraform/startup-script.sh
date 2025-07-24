e6342d619124868943e2acf8e132d29656d879e6
tanchi5454 <28645989+tanchi5454@users.noreply.github.com>
2025-07-24 12:24:52 +0900

    Update startup-script.sh

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 
 # MongoDB 4.4 (古いバージョン) のインストール
-apt-get update
+apt-get update || true
 apt-get install -y gnupg wget
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian bullseye/mongodb-org/4.4 main" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
