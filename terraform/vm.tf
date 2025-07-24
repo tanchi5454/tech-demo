8e6b8ff4dcc126893dd4cae0728632d9bef9ca19
tanchi5454 <28645989+tanchi5454@users.noreply.github.com>
2025-07-24 17:05:15 +0900

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -69,24 +69,6 @@ resource "google_project_iam_member" "vm_iam_secret_accessor" {
   member  = "serviceAccount:${google_service_account.mongodb_vm_sa.email}"
 }
 
-#  既に手動で作成したサービスアカウントをデータソースとして参照
-data "google_service_account" "github_actions_sa" {
-  account_id = "iac-operations-sa"
-}
-
-// ★★★ 追加: GitHub Actionsが使用するサービスアカウントをTerraformで管理 ★★★
-# resource "google_service_account" "iac-operations-sa" {
-#   account_id   = "iac-operations-sa"
-#   display_name = "Service Account for GitHub Actions"
-# }
-
-// ★★★ 追加: GitHub ActionsのサービスアカウントにSecret Managerへのアクセス権を付与 ★★★
-resource "google_project_iam_member" "github_actions_secret_accessor" {
-  project = var.project_id
-  role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
-}
-
 # MongoDB用のVMインスタンス
 resource "google_compute_instance" "mongodb_vm" {
   name         = "mongodb-vm"
